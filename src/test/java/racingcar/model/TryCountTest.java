db79d633c0dbdc8d90834c8d837fa0ab47fb33d5
hong-sile <gurwns9325@naver.com>
2022-12-08 20:47:10 +0900

    test(TryCount): TryCount 테스트 코드 추가
    tryCount가 1이상인지 확인하는 isPlayable 메소드 테스트 코드 추가
    tryCount를 하나 감소시키는 decrease 메소드 테스트 코드 추가

---
+++ src/test/java/racingcar/model/TryCountTest.java
@@ -0,0 +1,37 @@
+package racingcar.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TryCountTest {
+
+    @ParameterizedTest(name = "시도횟수가 {0}인 경우")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, Integer.MAX_VALUE})
+    void isPlayable_메소드는_시도횟수가_0보다_큰지_확인한다_count가_0보다_큰_경우(int count) {
+        TryCount tryCount = new TryCount(count);
+
+        Assertions.assertThat(tryCount.isPlayable()).isTrue();
+    }
+
+    @Test
+    void isPlayable_메소드는_시도횟수가_0보다_큰지_확인한다_count가_0인경우() {
+        int count = 0;
+        TryCount tryCount = new TryCount(count);
+
+        Assertions.assertThat(tryCount.isPlayable()).isFalse();
+    }
+
+    @Test
+    void decrease_메소드는_count를_하나_감소시킨다() {
+        //given
+        int count = 1;
+        TryCount tryCount = new TryCount(count);
+        //when
+        tryCount.decrease();
+        //then
+        Assertions.assertThat(tryCount.isPlayable())
+                .isFalse();
+    }
+}
