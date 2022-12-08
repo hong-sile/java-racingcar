c898cee95688b8ffa806e0b9f4943a981c0b9aeb
hong-sile <gurwns9325@naver.com>
2022-12-08 18:47:47 +0900

    feat(Controller): 1회 시도 후,시도 결과를 출력하는 기능 추가

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -5,25 +5,35 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 import racingcar.model.RacingGame;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingGameController {
     private RacingGame racingGame;
 
     public void run() {
-
+        initialCars();
+        initialTryCount();
+        gameStart();
     }
 
-    public void initialCars() {
+    private void gameStart() {
+        while (racingGame.isPlayable()) {
+            racingGame.tryOneTime();
+            OutputView.printTryResult(racingGame.getTryResult());
+        }
+    }
+
+    private void initialCars() {
         List<String> carNames = repeat(InputView::inputCarNames);
         repeat(racingGame::initialCars, carNames);
     }
 
-    public void initialTryCount() {
+    private void initialTryCount() {
         int tryCount = repeat(InputView::inputTryCount);
         repeat(racingGame::initialTryCount, tryCount);
     }
 
-    public <T> void repeat(Consumer<T> inputReader, T value) {
+    private <T> void repeat(Consumer<T> inputReader, T value) {
         try {
             inputReader.accept(value);
         } catch (IllegalArgumentException exception) {
@@ -32,7 +42,7 @@ public class RacingGameController {
         }
     }
 
-    public <T> T repeat(Supplier<T> inputReader) {
+    private <T> T repeat(Supplier<T> inputReader) {
         try {
             return inputReader.get();
         } catch (IllegalArgumentException exception) {
