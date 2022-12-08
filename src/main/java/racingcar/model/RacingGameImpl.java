f3793942f558b262121f3ff012ec869224d64d67
hong-sile <gurwns9325@naver.com>
2022-12-08 18:05:22 +0900

    feat(RacingGameImpl): 1회 시도해 자동차들을 움직이는 기능 구현

--- src/main/java/racingcar/model/RacingGameImpl.java
+++ src/main/java/racingcar/model/RacingGameImpl.java
@@ -1,7 +1,7 @@
 package racingcar.model;
 
 import java.util.List;
-import racingcar.model.dto.CarsDto;
+import racingcar.dto.CarsDto;
 
 public class RacingGameImpl implements RacingGame {
     private Cars cars;
