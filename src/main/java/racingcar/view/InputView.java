c32f6492e9cbdc1c07fad37f02e4fa36b03b5db4
hong-sile <gurwns9325@naver.com>
2022-12-08 20:07:02 +0900

    refactor: 메소드 네이밍 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String DELIMITER = ",";
 
-    public static List<String> inputCarName() {
+    public static List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         return Arrays.stream(input.split(DELIMITER)).collect(Collectors.toList());
@@ -17,9 +17,6 @@ public class InputView {
     public static int inputTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         int tryCount = inputIntValue();
-        if (tryCount < 0) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 0이상 이어야 합니다.");
-        }
         return tryCount;
     }
 
